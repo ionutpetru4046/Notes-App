beeff8506960c68de5fd37e029471daf116360b8
Coste Ionut Petru <ionut4046@yahoo.com>
2025-01-01 21:25:15 +0000

    SearchBar Component added to the Project

--- frontend/notes-app/src/components/Cards/ProfileInfo.jsx
+++ frontend/notes-app/src/components/Cards/ProfileInfo.jsx
@@ -1,7 +1,19 @@
-const ProfileInfo = () => {
+import { getInitials } from "../../utils/Helper"
+
+const ProfileInfo = ({onLogout}) => {
   return (
-    <div>
-        
+    <div className="flex items-center gap-3">
+      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 
+      font-medium bg-slate-100">
+        {getInitials("John William")}
+      </div>
+
+      <div>
+        <p className="text-sm font-medium">John William</p>
+        <button className="text-sm text-slate-700 underline" onClick={onLogout}>
+            Logout
+        </button>
+      </div>
     </div>
   )
 }
