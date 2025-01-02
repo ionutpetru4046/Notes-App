fa5d5bb428ed74fa88e7a80d9bd420fce1251bb0
Coste Ionut Petru <ionut4046@yahoo.com>
2025-01-02 19:29:41 +0000

    AddEditNotes component added to the project

---
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -0,0 +1,9 @@
+const AddEditNotes = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AddEditNotes
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from '../../components/Cards/NoteCard';
+import { MdAdd } from 'react-icons/md';
 
 const Home = () => {
   return (
@@ -7,18 +8,24 @@ const Home = () => {
      <Navbar />
 
      <div className="container mx-auto">
-
-      <NoteCard 
-        title="Meeting on 7th April" 
-        date="3rd April 2025" 
-        content="Meeting on 7th April Meeting on 7th April"
-        tags="#Meeting"
-        isPinned={true}
-        onEdit={() => {}}
-        onDelete={() => {}}
-        onPinNote={() => {}}
+       <div className="grid grid-cols-3 gap-4 mt-8">
+        <NoteCard 
+          title="Meeting on 7th April" 
+          date="3rd April 2025" 
+          content="Meeting on 7th April Meeting on 7th April"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
         />
+       </div>
      </div>
+
+     <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary 
+     hover:bg-blue-600 absolute right-10 bottom-10" onClick={() => {}}>
+        <MdAdd className="text-[32px] text-white" />
+     </button>
     </>
   )
 }
