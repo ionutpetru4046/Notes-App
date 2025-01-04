2bbadb918290eec0971a15caea34c7ee44e2c027
Coste Ionut Petru <ionut4046@yahoo.com>
2025-01-04 21:20:41 +0000

    working on backend creating the API endpoints

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,8 @@ app.get("/", (req, res) => {
     res.json({ data: "Hello from express" });
 });
 
-// Create Account
-app.post("/create-account", async (req, res) => {
+// Create Account API
+app.post("/create-account", async (req, res) => {   
 
     const { fullName, email, password } = req.body;
 
@@ -74,6 +74,51 @@ app.post("/create-account", async (req, res) => {
         message: "Registration successful",
     });
 });
+
+// Login API
+app.post("login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: "Email is required" });
+    }
+
+    if (!password) {
+        return res
+        .status(400)
+        .json({ message: "Password is required" });
+    }
+
+    const userInfo = await User.findOne({ email:email });
+
+    if (!userInfo) {
+        return res
+        .status(400)
+        .json({ message: "Invalid email or password" });
+    }
+
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo };
+        const accessToken = jwt.sign(user, process.env.ACCES_TOKEN_SECRET, {
+            expiresIn: "3600m",
+        });
+
+        return res.json({
+            error: false,
+            message: "Login successful",
+            email,
+            accessToken,
+        });
+
+    } else {
+        return res.status(400).json({
+         error: true,
+         message: "Invalid Credentials", 
+        });
+    }
+});
+
+// Add Note API
 
 app.listen(8000);
 
